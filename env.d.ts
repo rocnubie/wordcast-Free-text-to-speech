22e3f0319f2924ac4b1dfb711cbcd0da0eae756e
rocnubie <rocnubie@gmail.com>
2026-05-13 12:14:52 +0800

    initialize

--- env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_SITE_URL?: string;
-    NEXT_PUBLIC_PLAUSIBLE_DOMAIN?: string;
-    NEXT_PUBLIC_PLAUSIBLE_SCRIPT_URL?: string;
-    NEXT_PUBLIC_HF_HOST?: string;
-  }
-}
